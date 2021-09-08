8ba5d2de71ba1df0c315659510a7df04a4bdbd90
Aung Ko Ko Khant <aungkokokhant636@gmail.com>
2021-09-08 17:05:25 +0630

    database setup

--- resources/views/admin/contact-message.blade.php
+++ resources/views/admin/contact-message.blade.php
@@ -1,4 +1,27 @@
 @extends('layouts.adminlayout')
 @section('content')
     <h1>Contact message page</h1>
+    <table class="table table-hover">
+  <thead class="black white-text">
+    <tr>
+      <th scope="col">Id</th>
+      <th scope="col">UserName</th>
+      <th scope="col">Email</th>
+      <th scope="col">Message</th>
+      <th scope="col">Update</th>
+      <th scope="col">Delete</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+
+        <td>1</td>
+        <td>AKKK</td>
+        <td>Gmail@gmail.com</td>
+        <td>hi admin</td>
+        <td><button class="btn btn-sm green white-text">update</button></td>
+        <td><button class="btn btn-sm red white-text">delete</button></td>
+    </tr>
+  </tbody>
+</table>
 @endsection
--- resources/views/admin/manage-premium-users.blade.php
+++ resources/views/admin/manage-premium-users.blade.php
@@ -1,4 +1,29 @@
 @extends('layouts.adminlayout')
 @section('content')
-<h1>Manage premium user</h1>
+    <h1>Manage premium user</h1>
+    <table class="table table-hover">
+  <thead class="black white-text">
+    <tr>
+      <th scope="col">Id</th>
+      <th scope="col">UserName</th>
+      <th scope="col">Email</th>
+      <th scope="col">IsAdmin</th>
+      <th scope="col">IsPremiun</th>
+      <th scope="col">Update</th>
+      <th scope="col">Delete</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+
+        <td>1</td>
+        <td>AKKK</td>
+        <td>Gmail@gmail.com</td>
+        <td>true</td>
+        <td>true</td>
+        <td><button class="btn btn-sm green white-text">update</button></td>
+        <td><button class="btn btn-sm red white-text">delete</button></td>
+    </tr>
+  </tbody>
+</table>
 @endsection
